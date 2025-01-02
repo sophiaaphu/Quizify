7280c153cb615ba3f7b352366eeda1147a5b2d95
Sophia Phu <sophiatiffphu@gmail.com>
2025-01-02 16:30:08 -0600

    finished landing page UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,62 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { RiGoogleFill } from "react-icons/ri";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className=" flex flex-col gap-24 px-8 md:px-24 lg:px-36 pb-12">
+      <div className=" flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <h1 className=" font-bold text-4xl">A Better Way to Learn</h1>
+          <div>
+            <p className="hidden md:block">
+              Whether you&apos;re studying, teaching, or just curious, our web
+              app generates engaging,
+            </p>
+            <p className="hidden md:block">
+              customized quizzes in seconds - making learning smarter, faster,
+              and more fun
+            </p>
+            <p className="md:hidden">
+            Whether you&apos;re studying, teaching, or just curious, our web
+            app generates engaging, customized quizzes in seconds - making learning smarter, faster,
+            and more fun
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div>
+          <Button className=" bg-[#FF0000] hover:bg-[#FF5353]">
+            Login with Google
+            <RiGoogleFill/>
+          </Button>
+        </div>
+        <div className=" mt-4">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/landing-quiz-image.png"
+            alt="Description of the image"
+            width={500}
+            height={300}
+            className="w-full rounded-lg drop-shadow-lg"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+      </div>
+      <div className=" flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <h1 className=" font-bold text-4xl">Leveraging AI Power</h1>
+          <p>
+            Input your quiz topics and any relevant files and your quiz will be
+            created in seconds!
+          </p>
+        </div>
+        <div className=" mt-4">
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/landing-quiz-creation.png"
+            alt="Description of the image"
+            width={500}
+            height={300}
+            className="w-full rounded-lg drop-shadow-lg"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </div>
+    </main>
   );
 }
