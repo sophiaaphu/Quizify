0a5f17b847132a48e2b8a1938c1e16cff8e681ea
Sophia Phu <sophiatiffphu@gmail.com>
2025-01-02 22:19:23 -0600

    made navbar say hi user and changed fav ico

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,15 +1,27 @@
-import Link from "next/link";
-import Image from "next/image";
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useSession, signIn } from 'next-auth/react';
 import {
   NavigationMenu,
   NavigationMenuItem,
   navigationMenuTriggerStyle,
   NavigationMenuLink,
   NavigationMenuList,
-} from "@/components/ui/navigation-menu";
+} from '@/components/ui/navigation-menu';
+
 export default function Navbar() {
+  const { data: session } = useSession();
+
+  // Function to extract the first name from the full name
+  const getFirstName = (fullName: string | null | undefined): string => {
+    if (!fullName) return '';
+    return fullName.split(' ')[0];
+  };
+
   return (
-    <header className=" flex items-center justify-between py-4 px-8 lg:px-24 border-b">
+    <header className="flex items-center justify-between py-4 px-8 lg:px-24 border-b">
       <Link href="/">
         <Image
           src="/quizify-logo.png"
@@ -17,17 +29,24 @@ export default function Navbar() {
           width={160}
           height={80}
           className="w-[110px] lg:w-[140px]"
-        ></Image>
+        />
       </Link>
       <div>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/resources" legacyBehavior passHref>
-                <NavigationMenuLink className={`${navigationMenuTriggerStyle()} bg-[#FF5353] text-white`}>
-                  Log in
+              {session && session.user ? (
+                <span className="px-4 py-2 text-gray-700">
+                  Welcome, {getFirstName(session.user.name)}
+                </span>
+              ) : (
+                <NavigationMenuLink
+                  asChild
+                  className={`${navigationMenuTriggerStyle()} bg-[#FF5353] text-white`}
+                >
+                  <button onClick={() => signIn('google')}>Log in</button>
                 </NavigationMenuLink>
-              </Link>
+              )}
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
@@ -35,3 +54,4 @@ export default function Navbar() {
     </header>
   );
 }
+
